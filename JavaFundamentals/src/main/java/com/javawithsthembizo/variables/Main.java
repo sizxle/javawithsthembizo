fdc8c115af3dbac40d56b6a06294baf02a3a9c4c
Sthembizo Molefi <sthembizomolefi@gmail.com>
2026-01-14 09:59:11 +0200

    variables

---
+++ JavaFundamentals/src/main/java/com/javawithsthembizo/variables/Main.java
@@ -0,0 +1,10 @@
+package com.javawithsthembizo.variables;
+
+public class Main {
+
+    public static void main(String [] app){
+        int bmwWheels=4;
+        int vwWheels= bmwWheels;
+        System.out.print(vwWheels);
+    }
+}

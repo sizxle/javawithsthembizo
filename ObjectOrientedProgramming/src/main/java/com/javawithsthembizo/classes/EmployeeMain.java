1882882b3c3906bada1fb7fd5e333176027cda43
Sthembizo Molefi <sthembizomolefi@gmail.com>
2026-01-20 14:32:49 +0200

    encapsulation

--- ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/classes/EmployeeMain.java
+++ ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/classes/EmployeeMain.java
@@ -4,11 +4,9 @@ public class EmployeeMain {
 
     public static void main(String[] args) {
         var employee= new Employee();
-        employee.hourlyRate=10;
-        employee.baseSalary=50_000;
-
+        employee.setBaseSalary(50000);
+        employee.setHourlyRate(10);
         int wage=employee.calculateWage(30);
-
         System.out.println(wage);
     }
 }

922b96b6f2134a753e5df80cbe050cf4b5fecfdc
Sthembizo Molefi <sthembizomolefi@gmail.com>
2026-01-20 18:41:05 +0200

    coupling

---
+++ ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/classes/coupling/BrowserMain.java
@@ -0,0 +1,9 @@
+package com.javawithsthembizo.classes.coupling;
+
+public class BrowserMain {
+
+    public static void main(String[] args) {
+        var browser = new Browser();
+        browser.navigate("www.google.com");
+    }
+}

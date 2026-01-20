95ba9ab7ed98958707b5cea419cbcf13b0b1bb58
Sthembizo Molefi <sthembizomolefi@gmail.com>
2026-01-20 13:10:14 +0200

    Strings gets initailized to null // fix

--- ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/classes/TextBox.java
+++ ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/classes/TextBox.java
@@ -2,7 +2,7 @@ package com.javawithsthembizo.classes;
 
 public class TextBox {
 
-    public String text;
+    public String text="";//Reference Types gets initialized to null if no initial value is supplied
 
     public void setText(String text){
         this.text=text;

37447033e28b34b5940f2d2d30fac6b13547b5d4
Sthembizo Molefi <sthembizomolefi@gmail.com>
2026-01-20 18:55:53 +0200

    constructor overloading

--- ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/classes/Employee.java
+++ ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/classes/Employee.java
@@ -11,8 +11,16 @@ public class Employee {
         setHourlyRate(hourlyRate);
     }
 
+    public Employee(int baseSalary){
+     this(baseSalary,0);
+    }
+
     public int calculateWage(int extraHours){
-        return baseSalary+(extraHours*hourlyRate);
+        return baseSalary+(extraHours*getHourlyRate());
+    }
+
+    public int calculateWage(){
+        return calculateWage(0);
     }
 
     private void setBaseSalary(int baseSalary){
--- ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/classes/EmployeeMain.java
+++ ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/classes/EmployeeMain.java
@@ -4,7 +4,9 @@ public class EmployeeMain {
 
     public static void main(String[] args) {
         var employee= new Employee(50_000,20);
-        int wage=employee.calculateWage(10);
+        var employee3= new Employee(50_000);
+        int wage=employee.calculateWage();
         System.out.println(wage);
+
     }
 }

b9a260e4952ea13a0884e189496729efa066d6dd
Sthembizo Molefi <sthembizomolefi@gmail.com>
2026-01-20 10:03:52 +0200

    Class to represent a TextBox

---
+++ ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/classes/TextBox.java
@@ -0,0 +1,14 @@
+package com.javawithsthembizo.classes;
+
+public class TextBox {
+
+    public String text;
+
+    public void setText(String text){
+        this.text=text;
+    }
+
+    public void clear(){
+        this.text="";
+    }
+}

62c5f3076231acd17f3b0662dc8e3a4ac1603b7c
Sthembizo Molefi <sthembizomolefi@gmail.com>
2026-01-21 14:40:42 +0200

    reformatting

--- ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/ood/refactor/e1/Main.java
+++ ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/ood/refactor/e1/Main.java
@@ -5,14 +5,12 @@ import java.text.NumberFormat;
 
 public class Main {
 
-
     public static void main(String[] args) {
         double principle= new InputReader("Principle (R1k-R1M) :",1000,1_000_000).readNumber();
         float annualInterestRate=(float) new InputReader("Annual Interest Rate :",1,30).readNumber();
         short years =(short) new InputReader("Period (Years) :",1,30).readNumber();
 
         var loanCalculator =new LoanCalculator(principle,years,annualInterestRate);
-
         var loanReport=new LoanReport(loanCalculator);
 
         loanReport.printMonthlyPayments();

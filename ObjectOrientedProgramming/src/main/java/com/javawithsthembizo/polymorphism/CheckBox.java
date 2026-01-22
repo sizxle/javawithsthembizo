1bf1fbf243fb1e799e2d079bcbf48bdc8fcba521
Sthembizo Molefi <sthembizomolefi@gmail.com>
2026-01-22 19:23:43 +0200

    polymorphism

---
+++ ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/polymorphism/CheckBox.java
@@ -0,0 +1,9 @@
+package com.javawithsthembizo.polymorphism;
+
+public class CheckBox extends  UIControl{
+
+    @Override
+    public void render() {
+        System.out.println("render checkbox");
+    }
+}

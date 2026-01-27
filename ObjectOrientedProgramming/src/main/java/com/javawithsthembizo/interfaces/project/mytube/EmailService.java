ac1ef1d1e6a10912baae88bed40ab5f726d210c1
Sthembizo Molefi <sthembizomolefi@gmail.com>
2026-01-27 20:16:02 +0200

    project start point

---
+++ ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/interfaces/project/mytube/EmailService.java
@@ -0,0 +1,9 @@
+package com.javawithsthembizo.interfaces.project.mytube;
+
+public class EmailService {
+
+    public void sendEmail(User user) {
+        System.out.println("Notifying " + user.getEmail() + "...");
+        System.out.println("Done!\n");
+    }
+}

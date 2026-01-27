d02d7f602d329399e91413230e86b49bdfa6a98d
Sthembizo Molefi <sthembizomolefi@gmail.com>
2026-01-27 14:49:04 +0200

    Setter injection

---
+++ ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/interfaces/TaxCalculator2027.java
@@ -0,0 +1,8 @@
+package com.javawithsthembizo.interfaces;
+
+public class TaxCalculator2027 implements TaxCalculator{
+    @Override
+    public double calculateTax() {
+        return 0;
+    }
+}
--- ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/interfaces/TaxReport.java
+++ ObjectOrientedProgramming/src/main/java/com/javawithsthembizo/interfaces/TaxReport.java
@@ -13,4 +13,8 @@ public class TaxReport {
         var tax = calculator.calculateTax();
         System.out.println(tax);
     }
+
+    public void setCalculator(TaxCalculator calculator) {
+        this.calculator = calculator;
+    }
 }
